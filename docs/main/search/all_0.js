6cd3d72567462644879177f8a26f247926d8d282
Github Ubuntu action <noone@example.com>
2020-11-01 12:02:11 +0000

    Update API docs

--- docs/main/search/all_0.js
+++ docs/main/search/all_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['myveryownclass_0',['MyVeryOwnClass',['../classarosen_1_1Template_1_1MyVeryOwnClass.html',1,'arosen::Template']]]
+  ['myveryownclass_0',['MyVeryOwnClass',['../classMyVeryOwnClass.html',1,'']]],
+  ['myveryownclassrenamed_1',['MyVeryOwnClassRenamed',['../classarosen_1_1Template_1_1MyVeryOwnClassRenamed.html',1,'arosen::Template']]]
 ];
--- docs/main/search/classes_0.js
+++ docs/main/search/classes_0.js
@@ -1,4 +1,5 @@
 var searchData=
 [
-  ['myveryownclass_2',['MyVeryOwnClass',['../classarosen_1_1Template_1_1MyVeryOwnClass.html',1,'arosen::Template']]]
+  ['myveryownclass_3',['MyVeryOwnClass',['../classMyVeryOwnClass.html',1,'']]],
+  ['myveryownclassrenamed_4',['MyVeryOwnClassRenamed',['../classarosen_1_1Template_1_1MyVeryOwnClassRenamed.html',1,'arosen::Template']]]
 ];
